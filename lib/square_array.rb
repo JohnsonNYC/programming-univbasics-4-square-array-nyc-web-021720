32994c1066f2fbf16edd2bc465aea31aad224194
Johnson Kow <jkow95@gmail.com>
2020-01-30 15:06:58 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   index = 0
   
-  while index < 3 do 
+  while index < array.length do 
     newArray= []
     new_numbers = (array[index] ** 2)
     newArray.push(new_numbers)
